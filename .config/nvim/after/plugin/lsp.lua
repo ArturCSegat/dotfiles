dbc74f77ed3e61a94a3492b6f48641dff6af8bb3
ArturCSegat <arturcamerasegat@gmail.com>
2023-08-03 11:54:45 -0300

    better lsp bind for jumping files, definitions, references, and renaming

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -9,6 +9,29 @@ require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 -- Make sure you setup `cmp` after lsp-zero
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    -- Go back to previous file
+    vim.keymap.set('n', 'gb', "<C-6>")
+
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  end,
+})
+
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
@@ -24,19 +47,20 @@ require'lspconfig'.pylsp.setup{
   }
 }
 
-cmp.setup({
-  mapping = {
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select =true}),
-
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
-
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-  }
-})
+-- cmp.setup({
+--   mapping = {
+--     -- `Enter` key to confirm completion
+--     ['<CR>'] = cmp.mapping.confirm({select =true}),
+--
+--     -- Ctrl+Space to trigger completion menu
+--     ['<C-Space>'] = cmp.mapping.complete(),
+--
+--     -- Navigate between snippet placeholder
+--     ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+--     ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+--
+--   }
+-- })
 
 lsp.setup()
 
