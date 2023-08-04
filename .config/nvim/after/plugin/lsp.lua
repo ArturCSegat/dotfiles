7c1319120152227f55fbf4a6379c6f0a4c07772f
ArturCSegat <arturcamerasegat@gmail.com>
2023-08-04 00:12:45 -0300

    fix lsp confirm bind

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -47,20 +47,20 @@ require'lspconfig'.pylsp.setup{
   }
 }
 
--- cmp.setup({
---   mapping = {
---     -- `Enter` key to confirm completion
---     ['<CR>'] = cmp.mapping.confirm({select =true}),
---
---     -- Ctrl+Space to trigger completion menu
---     ['<C-Space>'] = cmp.mapping.complete(),
---
---     -- Navigate between snippet placeholder
---     ['<C-f>'] = cmp_action.luasnip_jump_forward(),
---     ['<C-b>'] = cmp_action.luasnip_jump_backward(),
---
---   }
--- })
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select =true}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+  }
+})
 
 lsp.setup()
 
