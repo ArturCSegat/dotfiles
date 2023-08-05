36228050b965801657e270cdf63857f3b05e15d9
ArturCSegat <arturcamerasegat@gmail.com>
2023-08-05 01:12:11 -0300

    closetag php

--- .config/nvim/after/plugin/closetag.lua
+++ .config/nvim/after/plugin/closetag.lua
@@ -1,6 +1,6 @@
 -- filenames like *.xml, *.html, *.xhtml, ...
 -- These are the file extensions where this plugin is enabled.
-vim.g.closetag_filenames = '*.html,*.xhtml,*.phtml'
+vim.g.closetag_filenames = '*.html,*.xhtml,*.phtml,*.php'
 
 -- filenames like *.xml, *.xhtml, ...
 -- This will make the list of non-closing tags self-closing in the specified files.
