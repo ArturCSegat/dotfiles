297a1cd1e20e78e0894c6e2d33807a01c61808e0
ArturCSegat <arturcamerasegat@gmail.com>
2023-08-12 14:38:28 -0300

    fixed not moving workspace 6

--- .config/i3/display_config.sh
+++ .config/i3/display_config.sh
@@ -10,6 +10,7 @@ i3-msg "workspace 2, move workspace to output eDP-1"
 i3-msg "workspace 3, move workspace to output eDP-1"
 i3-msg "workspace 4, move workspace to output eDP-1"
 i3-msg "workspace 5, move workspace to output eDP-1"
+i3-msg "workspace 6, move workspace to output eDP-1"
 i3-msg "workspace 7, move workspace to output eDP-1"
 
 i3-msg "workspace 1, move workspace to output HDMI-2"
@@ -17,5 +18,8 @@ i3-msg "workspace 2, move workspace to output HDMI-2"
 i3-msg "workspace 3, move workspace to output HDMI-2"
 i3-msg "workspace 4, move workspace to output HDMI-2"
 i3-msg "workspace 5, move workspace to output HDMI-2"
+i3-msg "workspace 6, move workspace to output HDMI-2"
 i3-msg "workspace 7, move workspace to output HDMI-2"
+
+i3-msg "workspace 1"
 
